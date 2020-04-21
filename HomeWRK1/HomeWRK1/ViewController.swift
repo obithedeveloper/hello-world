911bddd0e5d24c2b90f48876d703ab4228b8643e
Obinna <inlandobinna@gmail.com>
2020-04-21 16:20:26 -0500

    created initial project

---
+++ HomeWRK1/HomeWRK1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWRK1
+//
+//  Created by Obinna Ezekwesili on 4/21/20.
+//  Copyright © 2020 Obinna Ezekwesili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
